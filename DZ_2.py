057a2825e21aa14a6e38fe885f55c1202c601f82
Evgeniy <241126@mail.ru>
2022-09-03 16:21:59 +0300

    Add DZ_3

--- DZ_2.py
+++ DZ_2.py
@@ -1,10 +1,10 @@
 #Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
 print ('Введите числа X, Y, Z : ')
-X = int (input ())
-Y = int (input ())
-Z = int (input ())
-if (not (X or Y or Z) == (not X and not Y and not Z)):
+x = int (input ())
+y = int (input ())
+z = int (input ())
+if (not (x or y or z) == (not x and not y and not z)):
     print ('Утверждение истинно')
 else:
     print ('Утверждение ложно')
